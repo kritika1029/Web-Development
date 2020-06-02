f76baa77cd121a7c0646576f85e7400b210917e5
kritika1029 <60423390+kritika1029@users.noreply.github.com>
2020-06-02 22:07:41 +0530

    Updates

---
+++ precache-manifest.8bfb648c464a399b7c389d338bcd3ee7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67640caac632482f4a55d1e0069f05ab",
+    "url": "/Web-Development/index.html"
+  },
+  {
+    "revision": "d0b0f3cf548947fd3122",
+    "url": "/Web-Development/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e7e91edc8a04ecf8cd68",
+    "url": "/Web-Development/static/css/main.6edc3df7.chunk.css"
+  },
+  {
+    "revision": "d0b0f3cf548947fd3122",
+    "url": "/Web-Development/static/js/2.c801a21c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Web-Development/static/js/2.c801a21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e91edc8a04ecf8cd68",
+    "url": "/Web-Development/static/js/main.7e7ef4a5.chunk.js"
+  },
+  {
+    "revision": "dcac678f8097bc1d8415",
+    "url": "/Web-Development/static/js/runtime-main.f11f3ac1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web-Development/static/media/logo.5d5d9eef.svg"
+  }
+]);
